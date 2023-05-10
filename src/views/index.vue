59ddac60e77837451b50d354b011a254e311211f
lara huang <lara1105huang@gmail.com>
2023-05-11 00:34:48 +0800

    element-plus安裝自動導入

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,3 +1,5 @@
 <template>
-    <h1>欢迎vite+vue3+ts+pinia+element-plus+qiankun项目</h1>
+    <div>
+        <div>element Plus 安裝</div>
+    </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url';
 import { resolve } from 'path';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';/**/
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+     resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

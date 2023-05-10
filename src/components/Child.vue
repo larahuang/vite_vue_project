34a3f5ab644cb90935b3bd19b34b88df3017fd59
lara huang <lara1105huang@gmail.com>
2023-05-11 00:55:16 +0800

    語法糖父對子傳遞

---
+++ src/components/Child.vue
@@ -0,0 +1,26 @@
+<template>
+   <div>
+        <span>{{ props.name }}</span>
+        <ul>
+            <li v-for="(item, id) in list" :key="id">
+                {{ item.name }}
+            </li>
+        </ul>
+   </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+// 宣告props
+const props = defineProps({
+    name: {
+        type: String,
+        default: '11'
+    },
+    list: {
+        type: Array,
+        default: '11'
+    }
+
+})
+</script>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
-        <div>element Plus 安裝</div>
+        <div>父對子</div>
+         <child :name="name" :list="lists"/>  
     </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import Child from '../components/Child.vue'
+const name = ref('小叮噹')
+const lists = ref([
+    {id:110,name:'狂徒'},
+    { id: 111, name: '上班族' },
+    {id:112,name:'自由業'}
+])
+</script>

5bbb88f22a69dd73ba5211d0f5b2f7931a21a4f5
lara huang <lara1105huang@gmail.com>
2023-05-10 23:47:58 +0800

    設定環境變數

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { fileURLToPath, URL } from 'node:url';
+import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  envDir: resolve(__dirname, './env'),
 })

e6040951161cf82ceb90bcbc978eea5aa6088e8d
lara huang <lara1105huang@gmail.com>
2024-08-04 15:51:23 +0800

    provide &  jnject

--- src/components/Inject.vue
+++ src/components/Inject.vue
@@ -1,12 +1,10 @@
 <template>
-    <div v-show="sendIsShow">
-        <div>Model</div>
-        <h3>{{ sendTitle }}</h3>
-    </div>
+
 </template>
 
 <script setup lang="ts">
-import { inject } from 'vue'
+import { ref, inject } from 'vue';
+import Mode from '../components/mode.vue';
 const sendTitle = inject('title')
 const sendIsShow = inject('isShow')
 </script>
---
+++ src/views/There.vue
@@ -0,0 +1,19 @@
+<template>
+    <Inject></Inject>
+    <Mode></Mode>
+    <button class="mt-3" @click="changeShow">點擊</button>
+</template>
+
+<script setup lang="ts">
+import { ref,provide } from 'vue';
+import Inject from '../components/Inject.vue';
+import Mode from '../components/mode.vue';
+const title = ref('這是provide to jnject')
+const isShow = ref<boolean>(true);
+const changeShow = () => {
+    isShow.value = !isShow.value;
+}
+    provide('title', title)
+  provide('isShow',isShow)
+</script>
+
